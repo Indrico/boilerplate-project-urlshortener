960292e9124a70ec9531f25091acb9bbf7e68ed2
Indrico <indrico.jowensen@gmail.com>
2022-09-30 17:54:45 +0700

    Changed into MongoDB System

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
+
+const urlShortScehma = new Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+let UrlShort = mongoose.model('urlShort', urlShortScehma);
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { resolve } = require('path');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,7 +32,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/shorturl', function (req, res) {
+app.post('/api/shorturl', async function (req, res) {
   let inputUrl = req.body.url;
 
   let urlRegex = new RegExp(/(?<=(http|https):\/\/)[^\/]+/); // fragment locator
@@ -29,31 +42,29 @@ app.post('/api/shorturl', function (req, res) {
     return
   }
 
-  let rawdata = fs.readFileSync(path.resolve(__dirname, 'data.json'));
-  let data = JSON.parse(rawdata);
+  // let rawdata = fs.readFileSync(path.resolve(__dirname, 'data.json'));
+  // let data = JSON.parse(rawdata);
+  
   // Cek kalau datanya sudah ada.
-  let found = data.find((item) => item.original_url === req.body.url);
-  if (found) {
+  let found = await UrlShort.find({ original_url: req.body.url})
+
+  if (found.length > 0) {
     res.json(found);
   } else {
-    let last_id = data.reduce(
-      (acc, value) => (acc = acc > value.short_url ? acc : value.short_url),
-      0
-    );
-
+    let last_id = await UrlShort.find({}).sort({short_url: -1}).limit(1)
+    
+    if (last_id.length === 0) {
+      last_id = 0;
+    } else {
+      last_id = last_id[0].short_url
+    }
+    
     let output = {
       original_url: req.body.url,
       short_url: last_id + 1,
     };
 
-    // Push data baru ke data keseluruhan
-    data.push(output);
-
-    // Tulis data baru
-    fs.writeFileSync(
-      path.resolve(__dirname, 'data.json'),
-      JSON.stringify(data)
-    );
+    await UrlShort.create(output);
 
     res.json(output);
   }
@@ -66,7 +77,14 @@ app.get('/api/shorturl/:short_url', function (req, res) {
   let data = JSON.parse(rawdata);
   let found = data.find((item) => item.short_url === parseInt(short_url));
 
-  res.redirect(found.original_url);
+  if (found) {
+    res.redirect(found.original_url);
+  } else {
+    res.status = '404'
+    res.json({
+      error: "No short URL found for the given input"
+    })
+  }
 });
 
 app.listen(port, function () {
